b906f7b6e3d2355c0ff137aae83f516211ad89f1
Peter Kosyh <pkosyh@yandex.ru>
2024-03-04 01:49:05 +0300

    irc menu fix

--- irc.cpp
+++ irc.cpp
@@ -24,7 +24,7 @@ Irc::Irc(Screen &scr, Keyboard &kbd, WiFiClient &c, WiFiClientSecure &sc) :
 	view.h = ROWS - 1;
 	pass[0] = 0;
 	sprintf(server, "irc.oftc.net");
-	sprintf(nick, "esp-%02x", random()&0xff);
+	sprintf(nick, "esp-user");
 	sprintf(host, "esp32-c3");
 	channel[0] = 0;
 	sprintf(title, "IRC");
